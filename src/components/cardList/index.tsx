1ac56d71a5bbac1e262def93679b6fb433b6324e
WilianBVieira <wilianbernades42@gmail.com>
2022-12-13 18:10:36 -0300

    Feat: loading spinner

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -1,17 +1,24 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { IRAMProps, IReq } from "../../screens/Home";
+import React, { useEffect, useState } from "react";
+import { IRAMProps, IReq, IRickAndMorty } from "../../screens/Home";
 import Card from "../card";
+import LoadingComponent from "../loading";
 import { Container } from "./style";
 
 const CardList = ({ results }: IReq) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<IReq | null>();
+
   useEffect(() => {}, [results]);
 
   return (
     <Container>
-      <Grid templateColumns="repeat(4, 1fr)" gap={5}>
-        {results && results.map((l, i) => <Card data={l} key={i} />)}
-      </Grid>
+      <LoadingComponent loading={loading} />
+      {data && !loading && (
+        <Grid templateColumns="repeat(4, 1fr)" gap={5}>
+          {results && results.map((l, i) => <Card data={l} key={i} />)}
+        </Grid>
+      )}
     </Container>
   );
 };
